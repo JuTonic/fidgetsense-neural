569563bb5836bcd703fa10c5c263f29c0cc5f113
ton1c <sembl1@ya.ru>
2025-09-19 03:23:58 +0300

    model fixed

--- model.py
+++ model.py
@@ -3,8 +3,11 @@ import tensorflow as tf
 from utils import load_train_data
 from tensorflow.keras import layers, models
 from sklearn.model_selection import train_test_split
+from tensorflow.keras.utils import to_categorical
 
 X, y = load_train_data()
+
+y = to_categorical(y - 1, num_classes=4)
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
@@ -20,5 +23,8 @@ model.add(layers.Dense(4, activation='softmax'))
 model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
 model.summary()
 
-model.fit(X_train, y_train, epochs=10, batch_size=32, validation_data=(X_test, y_test))
+model.fit(X_train, y_train, epochs=50, batch_size=64, validation_data=(X_test, y_test))
 
+loss, accuracy = model.evaluate(X_test, y_test)
+
+print(f"Test Accuracy: {accuracy:.4f}")
